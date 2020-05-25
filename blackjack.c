75f151db38e5ce194845dcc8689c4f06e9db1a7f
motigohari <65549452+motigohari@users.noreply.github.com>
2020-05-25 00:58:52 -0700

    Update blackjack.c

--- blackjack.c
+++ blackjack.c
@@ -6,161 +6,198 @@
 
 
 
+
 int printRandoms(int lower, int upper);	
 
 
 int main(void)
 {
 	
-	int  player, bet1, bet2, bet3, bet4, bet5, ch1, ch2, ch3, diler;	
-   
+	int  player, diler, i, z, choice, split;	
+	 printf("\n********************\n"
+			"** Hello To Csino **\n"
+			"**      Royal     **\n"
+			"********************\n");
+			
 	printf("how many player (1-5)?\n");
 	scanf(" %d", &player);
-	printf("player %d\n", player);
-	//for (player = 0 ; player < 7 ; player++) 
 	 
 	if (player >= 6)
 	{
 		printf("The number of players exceeds the number allowed\n");
 		return 0;
 	}
-	if (player == 1)
+	else 
 	{
-		printf("What is your bet?(1-100)\n");
-		scanf("%d", &bet1);
-	}
-	else if (player == 2)
-	{
-		printf("What is your bet?(1-100)\n");
-		scanf("%d", &bet1);
-		printf("What is your bet?(1-100)\n");
-		scanf("%d", &bet2);
-	} 
-	else if (player == 3)
-	{
-		printf("What is your bet?(1-100)\n");
-		scanf("%d", &bet1);
-		printf("What is your bet?(1-100)\n");
-		scanf("%d", &bet2);
-		printf("What is your bet?(1-100)\n");
-		scanf("%d", &bet3);
-		
-	}
-	else if (player == 4)
-	{
-		printf("What is your bet?(1-100)\n");
-		scanf("%d", &bet1);
-		printf("What is your bet?(1-100)\n");
-		scanf("%d", &bet2);
-		printf("What is your bet?(1-100)\n");
-		scanf("%d", &bet3);
-		printf("What is your bet?(1-100)\n");
-		scanf("%d", &bet4);
-	}
-	else if (player == 5)
-	{
-		printf("What is your bet?(1-100)\n");
-		scanf("%d", &bet1);
-		printf("What is your bet?(1-100)\n");
-		scanf("%d", &bet2);
-		printf("What is your bet?(1-100)\n");
-		scanf("%d", &bet3);
-		printf("What is your bet?(1-100)\n");
-		scanf("%d", &bet4);
-		printf("What is your bet?(1-100)\n");
-		scanf("%d", &bet5);
-	}
-
-	printf("now we play :)\n");
-	printf("Hand # 1\n\n\n");
-	
-	int lower = 1, upper = 13, card, card2, card3; 
-        srand(time(0)); 
-		printf("Dealer's first card :\n");
-		diler = printRandoms(lower, upper);
-																		// Driver code
-																		// Use current time as  
-		printf("\nYour first two cards,\n");							// seed for random generator 
-		card = printRandoms(lower, upper); 
-		card2 = printRandoms(lower, upper);
-        card3= card+card2;
-            
-		printf("\nyou wont ander card?[y=1/n=0]"); 							
-		scanf ("%d", &ch1);	 
-    
-	if (ch1 == 0)
+		int bitim [player];
+		for ( i =1 ; i <= player ; i++)
 		{
-			printf("yor number is :%d", card3 );
-			printf("\nDealer's first card :%d", diler );
-			printf("\nDealer's second card :" );
-			card = printRandoms(lower, upper);
-			diler = card + diler;
-			printf("\nThe amount of the dealer's cards :%d",diler);
 			
-			
-			
+			printf("What is your bet?(1-100)\n");
+			scanf("%d", &bitim[i-1]);	
 		}
-       
-    else	 
-        {
+	}
+	
+	printf("now we play :)\n");
+	
+	for ( z = 1 ; z <= player ; z++)
+		
+		{
 			
-			int lower = 1, upper = 13; 
+			printf("\n\n*********\nplayer #%d\n\n\n", z);
+			int lower = 1, upper = 13, card, card2, card3, card4, card5; 
 			srand(time(0)); 
-			card= printRandoms(lower, upper);
-			card3= card+card3;
-			printf("\nyou wont ander card?[y=1/n=0]");
-			scanf ("%d", &ch2);
-			if (ch2 == 0)
-			{  
-				printf("yor number is :%d", card3 );
-				printf("\nDealer's first card :%d", diler );
-				printf("\nDealer's second card :" );
-				card = printRandoms(lower, upper);
-				diler = card + diler;
-				printf("\nThe amount of the dealer's cards :%d",diler);
-			}
-
-			else 
+			printf("Dealer's first card :\n");
+			diler = printRandoms(lower, upper);
+			
+			printf("\nYour first two cards,\n");							// seed for random generator 
+			card = printRandoms(lower, upper); 
+			card2 = printRandoms(lower, upper);
+			if ( card == card2 )
 			{
-				int lower = 1, upper = 13; 
-				srand(time(0)); 
-				card=printRandoms(lower, upper);
-				card3= card+card3;
-				printf("\nyou wont ander card?[y=1/n=0]");
-				scanf ("%d", &ch3);
-				
-				if (ch3 == 0)
-				 {
-					printf("yor number is :%d", card3 );
-					printf("\nDealer's first card :%d", diler );
-					printf("\nDealer's second card :" );
-					card = printRandoms(lower, upper);
-					diler = card + diler;
-					printf("\nThe amount of the dealer's cards :%d",diler);
-				 }
-				 else 
-				 {
-					 // printRandoms(int lower, int upper, int count);
+				printf("You want to split?[y=1/n=0]");
+				scanf("%d", &split);
+				if ( split == 1)
+				{
+					printf("\nplayer #%d\n",z);
+					printf("\nhand #1\n");
+					printf("%d\n", card);
+					card4 = printRandoms(lower, upper);
+					card5= card+card4;
+					printf("\nyou wont ander card?[y=1/n=0]"); 							
+					scanf ("%d", &choice);
+					if (choice == 1)
+					{
+					while ( choice > 0 )
+					{
 					int lower = 1, upper = 13; 
 					srand(time(0)); 
-					card=printRandoms(lower, upper);
-					card3= card+card3;
-					printf("\nThis is a last card : your number is %d",card3);
+					card= printRandoms(lower, upper);
+					card5= card+card5;
+					printf("\nyou wont ander(moti) card?[y=1/n=0]");
+					scanf ("%d", &choice);
+					if (choice == 1)
+					{
+						choice++;
+					}
+					else 
+					{
+						choice = 0 ;
+					}
+					}
+					}
+					printf("yor number is :%d", card5 );
 					printf("\nDealer's first card :%d", diler );
 					printf("\nDealer's second card :" );
 					card = printRandoms(lower, upper);
 					diler = card + diler;
 					printf("\nThe amount of the dealer's cards :%d",diler);
-				}
-				 
+					////////////////////////////////////////////////
+					printf("\nplayer #%d\n",z);
+					printf("\nhand #2\n");
+					printf("%d\n", card);
+					card4 = printRandoms(lower, upper);
+					card5= card+card4;
+					printf("\nyou wont ander card?[y=1/n=0]"); 							
+					scanf ("%d", &choice);
+					if (choice == 1)
+					{
+					while ( choice > 0 )
+					{
+					int lower = 1, upper = 13; 
+					srand(time(0)); 
+					card= printRandoms(lower, upper);
+					card5= card+card5;
+					printf("\nyou wont ander(moti) card?[y=1/n=0]");
+					scanf ("%d", &choice);
+					if (choice == 1)
+					{
+						choice++;
+					}
+					else 
+					{
+						choice = 0 ;
+					}
+					}
+					}
+					printf("yor number is :%d", card5 );
+					printf("\nDealer's first card :%d", diler );
+					printf("\nDealer's second card :" );
+					card = printRandoms(lower, upper);
+					diler = card + diler;
+					printf("\nThe amount of the dealer's cards :%d",diler);
 					
+				}
+				else
+				{
+					card3= card+card2;
+					printf("\nyou wont ander card?[y=1/n=0]"); 							
+					scanf ("%d", &choice);
+					
+				if (choice == 1)
+					{
+						while ( choice > 0 )
+						{
+						int lower = 1, upper = 13; 
+						srand(time(0)); 
+						card= printRandoms(lower, upper);
+						card3= card+card3;
+						printf("\nyou wont ander(moti) card?[y=1/n=0]");
+						scanf ("%d", &choice);
+						if (choice == 1)
+						{
+							choice++;
+						}
+						else 
+						{
+							choice = 0 ;
+						}
+						}
+					}
+						printf("yor number is :%d", card3 );
+						printf("\nDealer's first card :%d", diler );
+						printf("\nDealer's second card :" );
+						card = printRandoms(lower, upper);
+						diler = card + diler;
+						printf("\nThe amount of the dealer's cards :%d",diler);
+				}
 			}
-		}	
-		return 0 ;
-
+			else 
+			{
+				card3= card+card2;
+				printf("\nyou wont ander card?[y=1/n=0]"); 							
+				scanf ("%d", &choice);
+				
+				if (choice == 1)
+					{
+						while ( choice > 0 )
+						{
+						int lower = 1, upper = 13; 
+						srand(time(0)); 
+						card= printRandoms(lower, upper);
+						card3= card+card3;
+						printf("\nyou wont ander(moti) card?[y=1/n=0]");
+						scanf ("%d", &choice);
+						if (choice == 1)
+						{
+							choice++;
+						}
+						else 
+						{
+							choice = 0 ;
+						}
+						}
+					}
+						printf("yor number is :%d", card3 );
+						printf("\nDealer's first card :%d", diler );
+						printf("\nDealer's second card :" );
+						card = printRandoms(lower, upper);
+						diler = card + diler;
+						printf("\nThe amount of the dealer's cards :%d",diler);
+			}
+		}
+     return 0;
 }
-
-
 
 
 
@@ -200,9 +237,6 @@ int printRandoms(int lower, int upper)						//function to gut rundom number
 
   
 //Good game my frides
-
-
-
 
 
 
